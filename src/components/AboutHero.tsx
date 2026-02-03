71c323a0f5ef3e9532a402db54b63fdb72691a95
Ben Chigozie <115552805+benchigozie@users.noreply.github.com.>
2026-02-03 14:03:53 +0100

    chore: same padding changes.

--- src/components/AboutHero.tsx
+++ src/components/AboutHero.tsx
@@ -5,7 +5,7 @@ function AboutHero() {
             <div className='max-w-6xl mx-auto px-4'>
                 <h1 className='text-4xl md:text-6xl text-my-deep-blue font-bold mt-24 text-center'>About Us</h1>
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-10 md:mt-16">
-                    <div>
+                    <div className="px-5">
                         <p>MemeStructures was created to bring structure, discipline, and accountability to one of the most volatile corners of the crypto market. In an environment often driven by speculation and short-term hype, we focus on building a systematic approach to memecoin investing. An approach that prioritizes research, risk management, and long-term capital preservation.</p>
                         <br />
                         <p>We bridge the gap between promising memecoin developers and investors seeking exposure to high-conviction narratives without abandoning structure. Every opportunity we consider is subjected to rigorous due diligence, including narrative strength, liquidity dynamics, developer credibility, and downside risk. Our goal is not to chase trends, but to identify emerging narratives early and allocate capital with intention.</p>
